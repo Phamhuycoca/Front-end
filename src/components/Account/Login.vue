77615db2ede38cd1b5f7d736d084e6973e71d6e4
PhamHuy <phamkhachuy240702@gmail.com>
2024-01-23 15:40:28 +0700

    vadidator login

--- src/components/Account/Login.vue
+++ src/components/Account/Login.vue
@@ -1,3 +1,29 @@
+<script setup lang="js">
+import { ref } from 'vue';
+import { useForm } from 'vee-validate';
+import * as yup from 'yup';
+const visible = ref(false);
+const terms = ref(false);
+
+const { errors, handleSubmit, validate, defineField } = useForm({
+    validationSchema: yup.object({
+        email: yup.string().email().required(),
+        password: yup.string().min(6).required(),
+    }),
+});
+const onSubmit = handleSubmit(async values => {
+    try {
+        await validate();
+        console.log(values);
+    } catch (error) {
+        console.error('Validation failed:', error);
+    }
+});
+
+const [email, emailAttrs] = defineField('email');
+const [password, passwordAttrs] = defineField('password');
+</script>
+
 <template>
     <div>
         <v-card class="mx-auto pa-12 pb-8 my-6" variant="flat" max-width="500" rounded="lg">
@@ -5,10 +31,11 @@
             <div class="text-h4 text-center">
                 Đăng nhập
             </div>
-            <div class="text-subtitle-1 text-medium-emphasis">Email</div>
+            <div class="text-subtitle-1 text-medium-emphasis" v-bind="emailAttrs">Email</div>
 
-
-            <v-text-field density="compact" placeholder="Nhập email" variant="outlined"></v-text-field>
+            <v-text-field density="compact" v-model="email" placeholder="Nhập email" variant="outlined"></v-text-field>
+            <div v-show="errors.email" text-subtitle-1 text-medium-emphasis class="mb-4" style="color: red;">{{ errors.email
+            }}</div>
 
             <div class="text-subtitle-1 text-medium-emphasis d-flex align-center justify-space-between">
                 Mật khẩu
@@ -16,13 +43,15 @@
 
             <v-text-field :append-inner-icon="visible ? 'mdi-eye' : 'mdi-eye-off'" :type="visible ? 'text' : 'password'"
                 density="compact" placeholder="Nhập mật khẩu" variant="outlined"
-                @click:append-inner="visible = !visible"></v-text-field>
+                @click:append-inner="visible = !visible" v-model="password"></v-text-field>
+            <div v-show="errors.password" text-subtitle-1 text-medium-emphasis class="mb-4" style="color: red;">{{
+                errors.password }}</div>
             <div class="d-flex justify-space-between align-center">
                 <v-checkbox v-model="terms" color="secondary" label="Ghi nhớ Đăng nhập"></v-checkbox>
                 <a class="text-caption text-decoration-none text-blue mb-6">
                     Quên mật khẩu?</a>
             </div>
-            <v-btn block class="mb-8" color="#0F60FF" size="large">
+            <v-btn block class="mb-8" color="#0F60FF" size="large" @click="onSubmit">
                 Đăng nhập
             </v-btn>
             <div class="text-center">
@@ -34,16 +63,4 @@
         </v-card>
     </div>
 </template>
-
-<script>
-export default {
-    name: 'LoginView',
-    data() {
-        return {
-            visible: false,
-        }
-    }
-}
-</script>
-
 <style></style>
